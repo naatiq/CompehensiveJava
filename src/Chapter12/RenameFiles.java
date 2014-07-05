19081b23243dbb020ff110bd800da62f347f29ce
Naatiq@MacBook Air <naatiq@gmail.com>
2014-07-05 13:41:41 +0530

    Test

---
+++ src/Chapter12/RenameFiles.java
@@ -0,0 +1,10 @@
+package Chapter12;
+
+/**
+ * Created by naatiqullahmohammed on 05/07/14.
+ */
+public class RenameFiles {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+    }
+}
